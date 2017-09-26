5534defcfc91fbe5db4db81de69781f069705e7f
MrShepherd <mr.heimd4ll@gmail.com>
2017-09-26 16:43:02 +0800

    initial backend project without debug

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from config import config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    db.init_app(app)
+    # db.reflect(app=app)
+    tables = ['questions', 'user', 'testhistory', 'payhistory', 'gradehistory', 'invitecode']
+    op = getattr(db.Model.metedata, 'reflect')
+    op(bind=db.get_engine(app, 'weiqiumi'), views=True, only=tables)
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    return app
