58eb49814ecde15d856a630c4f05e5a6f807d980
MrShepherd <mr.heimd4ll@gmail.com>
2017-09-28 13:41:32 +0800

    finish userinfo api

--- app/main/apis.py
+++ app/main/apis.py
@@ -1,6 +1,8 @@
+from flask import request
+
 from . import main
 from .. import db
-from ..models import Questions
+from ..models import Questions, Users
 
 
 @main.route('/')
@@ -8,3 +10,19 @@ def index():
     # return '<h1>hello world</h1>'
     testcnt = db.session.query(Questions.title).first()
     return '<h1>%s</h1>' % testcnt
+
+
+@main.route('/api/userinfo', methods=['POST'])
+def userinfo():
+    print(request.values.get('openid'))
+    print(request.values.get('nickname'))
+    print(request.values.get('gender'))
+    print(request.values.get('city'))
+    print(request.values.get('province'))
+    print(request.values.get('avatarurl'))
+    user = Users(openid=request.values.get('openid'), nickname=request.values.get('nickname'), gender=request.values.get('gender'), city=request.values.get('city'),
+                 province=request.values.get('province'), avatarurl=request.values.get('avatarurl'))
+    db.session.merge(user)
+    user = Users(openid=request.values.get('openid'), dongqiudiID='1000')
+    db.session.merge(user)
+    return 'hello'
