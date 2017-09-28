18fadc05dcdc9173b6ac1f11f741e5442e5c8922
mr_shepherd <heimd4ll@yeah.net>
2017-09-28 20:24:36 +0800

    little fix

--- app/main/apis.py
+++ app/main/apis.py
@@ -14,15 +14,15 @@ def index():
 
 @main.route('/api/userinfo', methods=['POST'])
 def userinfo():
-    print(request.values.get('openid'))
-    print(request.values.get('nickname'))
-    print(request.values.get('gender'))
-    print(request.values.get('city'))
-    print(request.values.get('province'))
-    print(request.values.get('avatarurl'))
+    # print(request.values.get('openid'))
+    # print(request.values.get('nickname'))
+    # print(request.values.get('gender'))
+    # print(request.values.get('city'))
+    # print(request.values.get('province'))
+    # print(request.values.get('avatarurl'))
     user = Users(openid=request.values.get('openid'), nickname=request.values.get('nickname'), gender=request.values.get('gender'), city=request.values.get('city'),
                  province=request.values.get('province'), avatarurl=request.values.get('avatarurl'))
     db.session.merge(user)
-    user = Users(openid=request.values.get('openid'), dongqiudiID='1000')
-    db.session.merge(user)
+    # user = Users(openid=request.values.get('openid'), dongqiudiID='1000')
+    # db.session.merge(user)
     return 'hello'
